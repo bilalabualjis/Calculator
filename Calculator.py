35a658aad1d10f72777da89111b0fe3b719b1e79
bilalabualjis <75410397+bilalabualjis@users.noreply.github.com>
2021-03-25 20:48:26 -0700

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -8,6 +8,8 @@ wind.geometry("300x400")
 text = Entry(wind,font=("calibri", 16))
 text.pack(fill=X,padx=5,ipadx=5,ipady=5)
 
+def addToText(n):
+    text.insert(END,n)
 
 frame = Frame(wind)
 frame.pack(side=TOP,anchor=NW)
@@ -19,8 +21,12 @@ frame1 = Frame(frame)
 frame1.pack()
 
 #btn 1 to 3
-btn1 = Button(frame1,text="1",width=9,height=3)
+btn1 = Button(frame1,text="1",width=9,height=3, command=lambda:addToText("1"))
 btn1.pack(side=LEFT)
+btn2 = Button(frame1,text="2",width=9,height=3, command=lambda:addToText("2"))
+btn2.pack(side=LEFT)
+btn3 = Button(frame1,text="3",width=9,height=3, command=lambda:addToText("3"))
+btn3.pack(side=LEFT)
 
 
 wind.mainloop()
