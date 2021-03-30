262eb31768a1fce17f281c7790b7172d56967d13
bilal <abualjisbilal@gmail.com>
2021-03-29 22:55:37 -0700

    Added the rest of the buttons and project is complete

--- Calculator.py
+++ Calculator.py
@@ -10,6 +10,11 @@ text.pack(fill=X,padx=5,ipadx=5,ipady=5)
 
 def addToText(n):
     text.insert(END,n)
+def calculate():
+    result = eval(text.get())
+    text.delete(0,END)
+    text.insert(0,result)
+
 
 frame = Frame(wind)
 frame.pack(side=TOP,anchor=NW)
@@ -39,5 +44,38 @@ btn5 = Button(frame2,text="5",width=9,height=3, command=lambda:addToText("5"))
 btn5.pack(side=LEFT)
 btn6 = Button(frame2,text="6",width=9,height=3, command=lambda:addToText("6"))
 btn6.pack(side=LEFT)
+
+
+frame3 = Frame(frame)
+frame3.pack()
+
+#btn 7 to 9
+btn7 = Button(frame3,text="7",width=9,height=3, command=lambda:addToText("7"))
+btn7.pack(side=LEFT)
+btn8 = Button(frame3,text="8",width=9,height=3, command=lambda:addToText("8"))
+btn8.pack(side=LEFT)
+btn9 = Button(frame3,text="9",width=9,height=3, command=lambda:addToText("9"))
+btn9.pack(side=LEFT)
+
+frame4 = Frame(frame)
+frame4.pack()
+
+#btn 1 to 3
+btnpoint = Button(frame4,text=".",width=9,height=3, command=lambda:addToText("."))
+btnpoint.pack(side=LEFT)
+btnzero = Button(frame4,text="0",width=9,height=3, command=lambda:addToText("0"))
+btnzero.pack(side=LEFT)
+btneq = Button(frame4,text="=",width=9,height=3, command=lambda:calculate())
+btneq.pack(side=LEFT)
+
+#operators 
+btndiv = Button(rightFrame,text="/",width=9,height=3, command=lambda:addToText("/"))
+btndiv.pack()
+btnmul = Button(rightFrame,text="x",width=9,height=3, command=lambda:addToText("*"))
+btnmul.pack()
+btndif = Button(rightFrame,text="-",width=9,height=3, command=lambda:addToText("-"))
+btndif.pack()
+btnplus = Button(rightFrame,text="+",width=9,height=3, command=lambda:addToText("+"))
+btnplus.pack()
 
 wind.mainloop()
